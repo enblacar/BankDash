1959e043c8d43c106f83c0d6f745efeba70379e7
Enrique Blanco Carmona <e420a@dkfz-vpn2516.inet.dkfz-heidelberg.de>
2024-08-12 12:28:16 +0200

    Split calculator functionality into another streamlit app.

--- BankDash/__init__.py
+++ BankDash/__init__.py
@@ -3,8 +3,6 @@ from .utils import load_data, generate_color_map, update_plot_layout, get_plotly
 from .donut_plot import donut_plot
 from .heatmap_plot import heatmap_plot
 from .scatter_plot import scatter_plot
-from .calculators import calculate_future_value
-from .plot_future_values import plot_future_values
 from .treemap_plot import treemap_plot
 
 __all__ = ["load_data",
@@ -13,8 +11,6 @@ __all__ = ["load_data",
            "donut_plot",
            "heatmap_plot",
            "scatter_plot",
-           "calculate_future_value",
-           "plot_future_values",
            "treemap_plot",
            "get_plotly_colors",
            "display_palette_as_gradient"]
--- main.py
+++ main.py
@@ -7,11 +7,10 @@ def main():
     st.set_page_config(layout="wide", page_icon="random", page_title="Bank Eagle View")
 
     # Define pages
-    p1 = st.Page(page="./pages/1_page_donut_plot.py", title="Donut plot", icon=":material/query_stats:")
-    p2 = st.Page(page="./pages/2_page_heatmap_plot.py", title="Heatmap", icon=":material/query_stats:")
-    p3 = st.Page(page="./pages/3_page_scatter_plot.py", title="Scatter plot", icon=":material/query_stats:")
-    p4 = st.Page(page="./pages/4_page_treemap_plot.py", title="Treemap plot", icon=":material/query_stats:")
-    p5 = st.Page(page="./pages/5_page_calculate_future_value.py", title="Compound interest calculator", icon=":material/query_stats:")
+    p1 = st.Page(page="./page_directory/1_page_donut_plot.py", title="Donut plot", icon=":material/query_stats:")
+    p2 = st.Page(page="./page_directory/2_page_heatmap_plot.py", title="Heatmap", icon=":material/query_stats:")
+    p3 = st.Page(page="./page_directory/3_page_scatter_plot.py", title="Scatter plot", icon=":material/query_stats:")
+    p4 = st.Page(page="./page_directory/4_page_treemap_plot.py", title="Treemap plot", icon=":material/query_stats:")
 
     # Load and preprocess data (use caching to speed up reloads)
     @st.cache_data
@@ -69,7 +68,7 @@ def main():
     st.session_state.fontsize = fontsize_value
 
     # Install multipage navigation
-    pg = st.navigation(dict(Finances=[p1, p2, p3, p4], Calculators=[p5]))
+    pg = st.navigation(dict(Finances=[p1, p2, p3, p4]))
     pg.run()
 
 if __name__ == "__main__":
